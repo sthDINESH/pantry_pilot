09c55e4bd4b4db1e906ff987642066c20ddf13cf
sthDINESH <sthapit.dnsh@gmail.com>
2025-09-18 15:31:29 +0100

    update pantry item list view to filter based on authenticated user

--- pantry/views.py
+++ pantry/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import PantryItem
 
 # Create your views here.
 
 
-class PantryItemList(ListView):
+class PantryItemList(LoginRequiredMixin, ListView):
     """
+    Display pantry items for the currently logged-in user
     """
-    context_object_name = 'pantry_items'  # Renames object_list to pantry_items
-    queryset = PantryItem.objects.all()
+    context_object_name = 'pantry_items'
     template_name = 'pantry/pantry_item_list.html'
+    
+    def get_queryset(self):
+        """
+        Filter pantry items to only show items belonging to the current user
+        """
+        return PantryItem.objects.filter(user=self.request.user)
