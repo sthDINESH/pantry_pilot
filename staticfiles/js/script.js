08209d69caf7c1f313cf35278e6e5310c9dd2e72
sthDINESH <sthapit.dnsh@gmail.com>
2025-09-21 16:00:32 +0100

    add images to authentication forms

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DEBUG: Hello world!");
+
+    gsap.from(".auth-grid-container .square-card",
+    {
+        y: 1000,
+        duration: 1,
+        stagger: 0.1
+    }
+) 
 });
