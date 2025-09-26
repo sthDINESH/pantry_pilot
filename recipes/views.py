faf3fa24f8e4e7f7be2cab5d8fdeaebf9a854bb7
sthDINESH <sthapit.dnsh@gmail.com>
2025-09-26 16:43:10 +0100

    style the recipes page

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,6 @@ def recipes_list(request):
     Display a list of all recipes
     """
     context = {
-        'page_title': 'My Recipes',
+        'page_title': 'Recipes',
     }
     return render(request, 'recipes/recipes_list.html', context)
